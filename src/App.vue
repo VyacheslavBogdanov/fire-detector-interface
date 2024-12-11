1df8ab8f43f32e16e75c654c11a5aa8210ab8c7a
VyacheslavBogdanov <110238053+VyacheslavBogdanov@users.noreply.github.com>
2024-12-11 10:58:05 +0300

    Revert "Interface fix"

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,9 @@
 <template>
-	<h1 class="title">Детектор огня</h1>
 	<div id="app">
-		<CheckHealth :messageTypes="messageTypes" />
-		<FireDetection :messageTypes="messageTypes" />
+		<h1>FIRE DETECTOR APP INTERFACE</h1>
 	</div>
 </template>
 
-<script setup lang="ts">
-import CheckHealth from '@/components/CheckHealth/CheckHealth.vue';
-import FireDetection from '@/components/FireDetection/FireDetection.vue';
-import { ref, onMounted } from 'vue';
-import { fetchData } from './components/mocks/db';
+<script setup lang="ts"></script>
 
-const messageTypes = ref<{ class: string; message: string }[]>([]);
-
-const loadMessageTypes = async () => {
-	try {
-		messageTypes.value = await fetchData('/message-types');
-	} catch (error) {
-		console.error('Ошибка загрузки:', error);
-	}
-};
-
-onMounted(loadMessageTypes);
-</script>
-
-<style lang="scss" scoped>
-@import './styles/main.scss';
-
-.title {
-	margin: 1%;
-	color: rgba(100, 95, 95, 0.711);
-}
-
-#app {
-	width: 40%;
-	height: 500px;
-	display: flex;
-	align-items: center;
-	flex-direction: column;
-	font-family: sans-serif;
-	background-color: #fdfdfd;
-	margin: 1%;
-	padding: 20px;
-	border: $all-border-width solid $all-border-color;
-	border-radius: $all-border-radius;
-}
-</style>
+<style lang="scss" scoped></style>
--- src/components/utils/types.ts
+++
@@ -1,4 +0,0 @@
-interface MessageTypes {
-	class: string;
-	message: string;
-}
